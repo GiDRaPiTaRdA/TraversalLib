85620ea20815b7f721625e34ed24cb0fe34a3b25
GiDRaPiTaRdA <xxmaxdrivezz@gmail.com>
2018-01-02 16:47:56 +0200

    binary array receprive field traversal

--- Traversals/BinaryArrayTraversal.cs
+++ Traversals/BinaryArrayTraversal.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TraversalLib
 {
-    public class BinaryArrayTraversal
+    public static class BinaryArrayTraversal
     {
 
         public static void Traversal<T>(T[,] binaryArray, Action<T, int, int> action)
@@ -27,6 +27,34 @@ namespace TraversalLib
                 for (int y = 0; y < binaryArray1.GetLength(1); y++)
                 {
                     action.Invoke(binaryArray1[x, y], binaryArray2[x, y], x, y);
+                }
+            }
+        }
+
+        public static void Traversal<T>(this T[,] binaryArray, T[,] mask , Action<T[,], Tuple<int,int>> action)
+        {
+            int maskLengthAxisX = mask.GetLength(0);
+            int maskLengthAxisY = mask.GetLength(1);
+
+            int byteStepsAxisX = binaryArray.GetLength(0) - maskLengthAxisX;
+            int byteStepsAxisY = binaryArray.GetLength(1) - maskLengthAxisY;
+
+            for (int x = 0; x < byteStepsAxisX; x++)
+            {
+                for (int y = 0; y < byteStepsAxisY; y++)
+                {
+                    // Initialization of receptive field
+                    var receptiveField = new T[maskLengthAxisX,maskLengthAxisY];
+
+                    for (int mx = 0; mx < byteStepsAxisX; mx++)
+                    {
+                        for (int my = 0; my < byteStepsAxisY; my++)
+                        {
+                            receptiveField[mx, my] = binaryArray[x + mx, y + my];
+                        }
+                    }
+
+                    action.Invoke(receptiveField, new Tuple<int,int>(x, y));
                 }
             }
         }
